c1ce0a9e20f84f4fb3a25ef48d41a261247b21d4
Justin Davis <jrcd83@gmail.com>
2010-04-24 17:54:25 -0700

    Create all the helper functions we will need.

--- lib/ShipIt/Step/PodReadme.pm
+++ lib/ShipIt/Step/PodReadme.pm
@@ -3,6 +3,90 @@ package ShipIt::Step::PodReadme;
 use warnings;
 use strict;
 
+use File::Spec  qw();
+use Pod::Readme qw();
+use Carp        qw();
+
+our $VERSION = '0.01';
+
+sub new
+{
+    my $class = shift;
+    bless {}, $class;
+}
+
+sub run
+{
+    my ($state) = @_;
+
+    my $pod_path = _find_pod_file();
+}
+
+#---HELPER FUNCTION---
+# Finds the main POD file in order to run Pod::Readme on it.
+sub _find_pod_file
+{
+    my $modname   = _find_modname();
+    my $base_path = $modname;
+    $base_path    =~ tr{:}{/}s;
+
+    my ($pod_path) = ( grep { -e $_ }
+                       map { $base_path . $_ }
+                       qw/ .pod .pm / );
+
+    Carp::confess "Could not find the main module files for $modname"
+        unless $pod_path;
+
+    return $pod_path;
+}
+
+#---HELPER FUNCTION---
+sub _find_modname
+{
+    foreach ( qw/ Build.PL Makefile.PL / ) {
+        _find_modname_in_file( $_ ) if -f $_;
+    }
+
+    Carp::confess 'Could not find Makefile.PL or Build.PL';
+}
+
+my %REGEXS_FOR =
+    ( 'Makefile.PL' =>
+      [ qr{ NAME .+ (['"]) (.+?) \1 }xms, # ExtUtils::MakeMaker
+        qr{ \b name                       # Module::Install
+            (?: \s* [(] | \s+ )
+            (['"]) (.+?) \1 }xms ],
+      'Build.PL'    => 
+      [ qr{ \b name \b .+                 # Module::Build
+            (['"]) (.+?) \1 }xms ] );
+
+#---HELPER FUNCTION---
+# Partly copied from ShipIt::ProjectType::Perl
+# ... then, made super badass
+sub _find_modname_in_file
+{
+    my ($path) = @_;
+
+    open my $distfile, '<', $path,
+        or die qq{Can't open $path: $!};
+
+    my ($file) = reverse File::Spec->splitpath( $path );
+    my $modname;
+    my @regexs = @{ $REGEXS_FOR{ $file } };
+    while ( my $line = <$distfile> ) {
+        for my $regex ( @regexs ) {
+            return $2 if $line =~ $regex;
+        }
+    }
+
+    close $distfile;
+    Carp::confess "Could not find the module name in $path";
+}
+
+1; # End of ShipIt::Step::PodReadme
+
+__END__
+
 =head1 NAME
 
 ShipIt::Step::PodReadme - The great new ShipIt::Step::PodReadme!
@@ -10,11 +94,6 @@ ShipIt::Step::PodReadme - The great new ShipIt::Step::PodReadme!
 =head1 VERSION
 
 Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
 
 =head1 SYNOPSIS
 
@@ -26,27 +105,6 @@ Perhaps a little code snippet.
 
     my $foo = ShipIt::Step::PodReadme->new();
     ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
@@ -106,5 +164,3 @@ See http://dev.perl.org/licenses/ for more information.
 
 
 =cut
-
-1; # End of ShipIt::Step::PodReadme
